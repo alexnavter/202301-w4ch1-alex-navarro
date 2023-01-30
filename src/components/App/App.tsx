ed2b9e188209dd0c7e2183e54909f92092d918b5
Alex Navarro <alexnavter@gmail.com>
2023-01-30 21:04:46 +0100

    Create Info component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
+import Info from "../Info/Info";
 import Title from "../Title/Title";
 
 function App() {
   const appTitle = "The pointing gentlemen";
+  const infoText = "0 gentlemen pointing at you";
 
   return (
     <div className="container">
       <header className="main-header">
         <Title title={appTitle} />
+        <Info text={infoText} />
       </header>
     </div>
   );
